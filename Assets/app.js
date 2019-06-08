9dcb3eae659e912d1eb3a4c5c8faaab8615c5e9d
Katie Vogel <kvog983@gmail.com>
2019-06-07 23:08:01 -0400

    registerring button click one at a time, not efficient code, find better way

--- Assets/app.js
+++ Assets/app.js
@@ -1,4 +1,4 @@
-  // Your web app's Firebase configuration
+  // Firebase configuration code
   var firebaseConfig = {
     apiKey: "AIzaSyA67SZGzCLIPI4YnxZgeP9EDwCRgVm_mPY",
     authDomain: "bncmultiplayer07june2019.firebaseapp.com",
@@ -8,10 +8,34 @@
     messagingSenderId: "686218092147",
     appId: "1:686218092147:web:9c287d2e934102ba"
   };
-  // Initialize Firebase
+  
   firebase.initializeApp(firebaseConfig);
 
-  var databaseReference = firebase.database();
+  var database = firebase.database();
+
+  //Variables
+  var wins1 = 0;
+  var wins2 = 0;
+  var losses1 = 0;
+  var losses2 = 0;
+  var ties = 0;
+  var player1Choice;
+  var player2Choice;
+
+$(".player1 .bear").on("click", function () {
+    var args = {
+        name: "Player 1",
+        choice: "bear",
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+    database.ref().push(args); 
+});
+
+  database.ref().on("child_added", function (snapshot) {
+    var record = snapshot.val();
+    var player1Selection = $('#player1-choice').append(player1Choice);
+    player1Selection.innerHTML = '<div>Player 1 has chosen <span\ id="player1-choice">' + player1Choice + '</span></div>';
+});
 
 
 
@@ -24,17 +48,14 @@
 
 
 
+//Scoring
+  if ((player1Choice === "bear") || (player1Choice === "ninja") || (player1Choice === "cowboy")) {
 
-
-
-
-  if ((player1 === "bear") || (player1 === "ninja") || (player1 === "cowboy")) {
-
-    if ((player1 === "bear" && player2 === "ninja") ||
-      (player1 === "ninja" && player2 === "cowboy") || 
-      (player1 === "cowboy" && player2 === "bear")) {
+    if ((player1Choice === "bear" && player2Choice === "ninja") ||
+      (player1Choice === "ninja" && player2Choice === "cowboy") || 
+      (player1Choice === "cowboy" && player2Choice === "bear")) {
       wins++;
-    } else if (player1 === player2) {
+    } else if (player1Choice === player2Choice) {
       ties++;
     } else {
       losses++;
